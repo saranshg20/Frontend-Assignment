95252fbfa1227cf1a58973a43b57983f19372b93
saranshg20 <saranshg20@iitk.ac.in>
2024-03-12 20:35:03 +0530

    Update react-router-dom imports and add new utils.js file

--- Metrics_Visualizer_And_Logger/src/App.jsx
+++ Metrics_Visualizer_And_Logger/src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Metrics from "./pages/Metrics";
 import Logs from "./pages/Logs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [duration, setDuration] = useState("5");
@@ -9,8 +9,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/metrics" element={<Metrics duration={duration} setDuration={setDuration} />} />
-        <Route path="/logs" element={<Logs duration={duration} setDuration={setDuration} />} />
+        <Route path="/" element={<Navigate to="/metrics" />} />
+        <Route path="/metrics" element={<Metrics duration={duration} setDuration={setDuration}/>} />
+        <Route path="/logs" element={<Logs duration={duration} setDuration={setDuration}/>} />
       </Routes>
     </BrowserRouter>
   )
---
+++ Metrics_Visualizer_And_Logger/src/utils/utils.js
@@ -0,0 +1,8 @@
+export const timeMap = {
+    "5": "Last 5 minutes",
+    "15": "Last 15 minutes", 
+    "30": "Last 30 minutes", 
+    "60": "Last 1 hour", 
+    "180": "Last 3 hours", 
+    "360": "Last 6 hours"
+}
